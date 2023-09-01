bfc915fb37456287a6feff6ecfec64556f41de68
JesseChen <jesse_002@163.com>
2023-09-01 21:51:49 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from armv8_a_study!\n";
+}
---
+++ tests/1assembly.cpp
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+TEST(assembly_test, e2e) {
+  ASSERT_EQ(42, 42);
+}
